73701465710ba848a146477d8c330b52dc42c26b
Rosie J-H <rosiej-h@baumann.fritz.box>
2023-01-16 16:15:59 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Worald");
+        System.out.println("wibble wobble");
+        System.out.println("Keyboard shortcuttttssss");
+    }
+}
